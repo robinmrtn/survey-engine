7aad6f056d3f603bb6655a81a7c4bfb54b1c0eec
robin <r.martini.robin@gmail.com>
2021-03-18 00:25:46 +0100

    added equals and hashCode method to DateRange

--- src/main/java/com/roal/jsurvey/entity/DateRange.java
+++ src/main/java/com/roal/jsurvey/entity/DateRange.java
@@ -31,4 +31,22 @@ public class DateRange {
     public boolean isBetween(LocalDate date) {
         return date.isAfter(startDate.minusDays(1)) && date.isBefore(endDate.plusDays(1));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DateRange dateRange = (DateRange) o;
+
+        if (!startDate.equals(dateRange.startDate)) return false;
+        return endDate.equals(dateRange.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = startDate.hashCode();
+        result = 31 * result + endDate.hashCode();
+        return result;
+    }
 }
