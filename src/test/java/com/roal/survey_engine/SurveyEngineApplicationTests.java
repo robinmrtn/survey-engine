996eaec4cffcdb60193bfd6cad21d7c5388b10fb
robin.martini <r.martini.robin@gmail.com>
2021-08-17 16:28:03 +0200

    Application test with random port

--- src/test/java/com/roal/survey_engine/SurveyEngineApplicationTests.java
+++ src/test/java/com/roal/survey_engine/SurveyEngineApplicationTests.java
@@ -1,12 +1,13 @@
 package com.roal.survey_engine;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SurveyEngineApplicationTests {
 
 	@Test
 	void contextLoads() {
-		SurveyEngineApplication.main(new String[]{});
 	}
 
 }
