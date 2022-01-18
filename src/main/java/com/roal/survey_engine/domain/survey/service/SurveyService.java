c7cbb7573c43eb3f78c5ff58b8123f08de6658fd
robin.martini <r.martini.robin@gmail.com>
2022-01-18 13:59:06 +0100

    SurveyController returns only DTOs

--- src/main/java/com/roal/survey_engine/domain/survey/service/SurveyService.java
+++ src/main/java/com/roal/survey_engine/domain/survey/service/SurveyService.java
@@ -1,5 +1,7 @@
 package com.roal.survey_engine.domain.survey.service;
 
+import com.roal.survey_engine.domain.survey.dto.SurveyDto;
+import com.roal.survey_engine.domain.survey.dto.SurveyDtoMapper;
 import com.roal.survey_engine.domain.survey.dto.SurveyListElementDto;
 import com.roal.survey_engine.domain.survey.entity.Campaign;
 import com.roal.survey_engine.domain.survey.entity.Survey;
@@ -17,26 +19,37 @@ public class SurveyService {
 
     private final CampaignRepository campaignRepository;
 
-    public SurveyService(SurveyRepository surveyRepository, CampaignRepository campaignRepository) {
+    private final SurveyDtoMapper surveyDtoMapper;
+
+    public SurveyService(SurveyRepository surveyRepository,
+                         CampaignRepository campaignRepository,
+                         SurveyDtoMapper surveyDtoMapper) {
         this.surveyRepository = surveyRepository;
         this.campaignRepository = campaignRepository;
+        this.surveyDtoMapper = surveyDtoMapper;
     }
 
     public Survey save(Survey survey) {
         return surveyRepository.save(survey);
     }
 
+    public SurveyDto saveDto(SurveyDto surveyDto) {
+        Survey survey = surveyDtoMapper.dtoToEntity(surveyDto);
+        surveyRepository.save(survey);
+        return surveyDtoMapper.entityToDto(survey);
+    }
+
     public Survey findSurveyById(long id) {
         return surveyRepository.findById(id).orElseThrow(() -> new SurveyNotFoundException(id));
     }
 
-    public Survey findSurveyByCampaignId(long campaignId) {
+    public SurveyDto findSurveyByCampaignId(long campaignId) {
         var campaign = campaignRepository
                 .findById(campaignId).orElseThrow(() -> new SurveyNotFoundException(campaignId));
         if (campaign.getSurvey() == null) {
             throw new SurveyNotFoundException(campaignId);
         }
-        return campaign.getSurvey();
+        return surveyDtoMapper.entityToDto(campaign.getSurvey());
     }
 
     public Page<SurveyListElementDto> getPublicAndActiveSurveys(Pageable pageable) {
