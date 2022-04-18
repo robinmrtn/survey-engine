a183bfb0ca24623847e0543f59cd8c8efe49e074
robin <r.martini.robin@gmail.com>
2022-04-18 22:52:41 +0200

    add isAdmin flag to UserDto

--- src/main/java/com/roal/survey_engine/domain/user/dto/UserDtoMapper.java
+++ src/main/java/com/roal/survey_engine/domain/user/dto/UserDtoMapper.java
@@ -2,6 +2,7 @@ package com.roal.survey_engine.domain.user.dto;
 
 import com.roal.survey_engine.domain.user.entity.Role;
 import com.roal.survey_engine.domain.user.entity.User;
+import com.roal.survey_engine.security.AuthenticationFacade;
 import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -14,21 +15,28 @@ import java.util.stream.Collectors;
 public class UserDtoMapper {
 
     private final Hashids userHashids;
+    private final AuthenticationFacade authenticationFacade;
 
-    public UserDtoMapper(@Qualifier("userHashids") Hashids userHashids) {
+    public UserDtoMapper(@Qualifier("userHashids") Hashids userHashids, AuthenticationFacade authenticationFacade) {
         this.userHashids = userHashids;
+        this.authenticationFacade = authenticationFacade;
     }
 
     public User dtoToEntity(UserRegistrationDto userDto) {
-        List<Role> roles = userDto.roles().stream().map(Role::new).toList();
+        List<Role> roles = userDto.roles()
+            .stream()
+            .map(Role::new)
+            .toList();
         return new User(userDto.username(), userDto.password(), roles);
     }
 
     public UserDto entityToDto(User user) {
-        Set<String> roles = user.getRoles().stream()
-                .map(Role::getName)
-                .collect(Collectors.toSet());
+        Set<String> roles = user.getRoles()
+            .stream()
+            .map(Role::getName)
+            .collect(Collectors.toSet());
+
         return new UserDto(userHashids.encode(user.getId()),
-                user.getUsername(), roles);
+            user.getUsername(), roles, authenticationFacade.isAdmin());
     }
 }
