dc12ec4c8678d8e3854f0616a4d921e7aeb1b78d
robin.martini <r.martini.robin@gmail.com>
2022-05-04 14:53:05 +0200

    migrate ID type from long (prmitive) to Long

--- src/main/java/com/roal/survey_engine/domain/user/entity/User.java
+++ src/main/java/com/roal/survey_engine/domain/user/entity/User.java
@@ -48,11 +48,11 @@ public class User {
         this.username = username;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
