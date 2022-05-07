2cbd1ae9204cb8d0cfedaf4f1aef62cce3607143
robin <r.martini.robin@gmail.com>
2022-05-07 15:41:06 +0200

    add workspace creation to startup script

--- src/main/java/com/roal/survey_engine/runner/StartupCommandLineRunner.java
+++ src/main/java/com/roal/survey_engine/runner/StartupCommandLineRunner.java
@@ -3,12 +3,14 @@ package com.roal.survey_engine.runner;
 import com.roal.survey_engine.domain.survey.entity.Campaign;
 import com.roal.survey_engine.domain.survey.entity.Survey;
 import com.roal.survey_engine.domain.survey.entity.SurveyPage;
+import com.roal.survey_engine.domain.survey.entity.Workspace;
 import com.roal.survey_engine.domain.survey.entity.question.ClosedQuestion;
 import com.roal.survey_engine.domain.survey.entity.question.ClosedQuestionAnswer;
 import com.roal.survey_engine.domain.survey.entity.question.OpenNumericQuestion;
 import com.roal.survey_engine.domain.survey.entity.question.OpenTextQuestion;
 import com.roal.survey_engine.domain.survey.repository.CampaignRepository;
 import com.roal.survey_engine.domain.survey.repository.SurveyRepository;
+import com.roal.survey_engine.domain.survey.repository.WorkspaceRepository;
 import com.roal.survey_engine.domain.user.dto.UserRegistrationDto;
 import com.roal.survey_engine.domain.user.entity.Role;
 import com.roal.survey_engine.domain.user.repository.RoleRepository;
@@ -33,23 +35,27 @@ class StartupCommandLineRunner implements CommandLineRunner {
     private final UserService userService;
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final WorkspaceRepository workspaceRepository;
 
     StartupCommandLineRunner(SurveyRepository surveyRepository,
                              CampaignRepository campaignRepository,
                              UserService userService,
-                             RoleRepository roleRepository, UserRepository userRepository) {
+                             RoleRepository roleRepository,
+                             UserRepository userRepository,
+                             WorkspaceRepository workspaceRepository) {
         this.surveyRepository = surveyRepository;
         this.campaignRepository = campaignRepository;
         this.userService = userService;
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.workspaceRepository = workspaceRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         addRoles();
-        addSurvey();
         addUsers();
+        addSurvey();
     }
 
     private void addUsers() {
@@ -66,16 +72,22 @@ class StartupCommandLineRunner implements CommandLineRunner {
     }
 
     private void addSurvey() {
+
+        var workspace = new Workspace("default Workspace");
+
+        workspaceRepository.save(workspace);
+
         var survey = new Survey()
-                .setDescription("This is a Survey Description")
-                .setTitle("This is a Survey Title")
-                .addSurveyPage(new SurveyPage()
-                        .addSurveyElement(new OpenTextQuestion("This is an open question?"))
-                        .addSurveyElement(new ClosedQuestion("This is an closed question")
-                                .addAnswer(new ClosedQuestionAnswer("First answer"))
-                                .addAnswer(new ClosedQuestionAnswer("Second answer"))))
-                .addSurveyPage(new SurveyPage()
-                        .addSurveyElement(new OpenTextQuestion("This is another open question"))
+            .setDescription("This is a Survey Description")
+            .setTitle("This is a Survey Title")
+            .setWorkspace(workspace)
+            .addSurveyPage(new SurveyPage()
+                .addSurveyElement(new OpenTextQuestion("This is an open question?"))
+                .addSurveyElement(new ClosedQuestion("This is an closed question")
+                    .addAnswer(new ClosedQuestionAnswer("First answer"))
+                    .addAnswer(new ClosedQuestionAnswer("Second answer"))))
+            .addSurveyPage(new SurveyPage()
+                .addSurveyElement(new OpenTextQuestion("This is another open question"))
                 .addSurveyElement(new OpenNumericQuestion("This is a numeric question")));
         var campaign = new Campaign()
             .setSurvey(survey)
