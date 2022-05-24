1422cedc523c4cbbccbd1c1691c18eb36ba2a6b3
robin.martini <r.martini.robin@gmail.com>
2022-05-24 16:02:28 +0200

    migrate SurveyListElementDto to record

--- src/main/java/com/roal/survey_engine/domain/survey/dto/survey/SurveyListElementDto.java
+++ src/main/java/com/roal/survey_engine/domain/survey/dto/survey/SurveyListElementDto.java
@@ -1,53 +1,5 @@
 package com.roal.survey_engine.domain.survey.dto.survey;
 
-public class SurveyListElementDto {
-
-    private String id;
-    private String name;
-    private String description;
-
-    public SurveyListElementDto(String id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SurveyListElementDto that = (SurveyListElementDto) o;
-
-        return id != null ? id.equals(that.id) : that.id == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
+public record SurveyListElementDto(String id, String name, String description) {
 }
+
