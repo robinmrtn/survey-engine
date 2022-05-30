c001ae2a70b876df3501f2a921d4488acb6a9af6
robin <r.martini.robin@gmail.com>
2022-05-31 01:50:04 +0200

    add Email and more validation to UserRegistrationDto

--- src/main/java/com/roal/survey_engine/domain/user/dto/UserRegistrationDto.java
+++ src/main/java/com/roal/survey_engine/domain/user/dto/UserRegistrationDto.java
@@ -1,8 +1,13 @@
 package com.roal.survey_engine.domain.user.dto;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
-public record UserRegistrationDto(@NotBlank String username, @NotBlank String password, @NotEmpty Set<String> roles) {
+public record UserRegistrationDto(@Size(min = 3, max = 32) String username,
+                                  @Size(min = 8) String password,
+                                  @Size(min = 8) String passwordRepeated,
+                                  @Email String email,
+                                  @NotEmpty Set<String> roles) {
 }
