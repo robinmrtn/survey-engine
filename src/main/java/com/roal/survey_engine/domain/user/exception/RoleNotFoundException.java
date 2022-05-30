1d2b4639c019a27790652270e0b6aabd0060af32
robin <r.martini.robin@gmail.com>
2022-05-31 01:28:43 +0200

    add Response Code information to exceptions

--- src/main/java/com/roal/survey_engine/domain/user/exception/RoleNotFoundException.java
+++ src/main/java/com/roal/survey_engine/domain/user/exception/RoleNotFoundException.java
@@ -1,5 +1,9 @@
 package com.roal.survey_engine.domain.user.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RoleNotFoundException extends RuntimeException {
     public RoleNotFoundException(String roleName) {
         super("Role '" + roleName + "' not found");
--- src/main/java/com/roal/survey_engine/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/roal/survey_engine/domain/user/exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.roal.survey_engine.domain.user.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String id) {
         super("User '" + id + "' not found");
