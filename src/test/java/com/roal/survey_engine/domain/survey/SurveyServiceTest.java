5d8485233ad4267783813aa267f0c2b6c9d579e9
robin.martini <r.martini.robin@gmail.com>
2022-06-14 16:29:21 +0200

    make service test unit test so application context is not needed

--- src/test/java/com/roal/survey_engine/domain/survey/SurveyServiceTest.java
+++ src/test/java/com/roal/survey_engine/domain/survey/SurveyServiceTest.java
@@ -1,50 +1,47 @@
 package com.roal.survey_engine.domain.survey;
 
-import com.roal.survey_engine.domain.response.dto.ElementResponseDto;
-import com.roal.survey_engine.domain.response.dto.OpenQuestionResponseDto;
-import com.roal.survey_engine.domain.response.dto.SurveyResponseDto;
-import com.roal.survey_engine.domain.survey.entity.Campaign;
+import com.roal.survey_engine.domain.survey.dto.survey.SurveyListElementDto;
 import com.roal.survey_engine.domain.survey.entity.Survey;
 import com.roal.survey_engine.domain.survey.exception.SurveyNotFoundException;
-import com.roal.survey_engine.domain.survey.repository.CampaignRepository;
 import com.roal.survey_engine.domain.survey.repository.SurveyRepository;
+import com.roal.survey_engine.domain.survey.service.CampaignService;
 import com.roal.survey_engine.domain.survey.service.SurveyService;
 import com.roal.survey_engine.security.AuthenticationFacade;
-import org.junit.jupiter.api.Disabled;
+import org.hashids.Hashids;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+
+@ExtendWith(MockitoExtension.class)
 @Transactional
 class SurveyServiceTest {
 
-    @Autowired
-    private SurveyService surveyService;
-
-    @MockBean
-    private SurveyRepository surveyRepository;
-
-    @MockBean
-    private CampaignRepository campaignRepository;
-
-    @MockBean
+    @Mock
     AuthenticationFacade authenticationFacade;
+    @Mock
+    CampaignService campaignService;
+    @InjectMocks
+    private SurveyService surveyService;
+    @Mock
+    private SurveyRepository surveyRepository;
+    @Spy
+    private Hashids surveyHashids = new Hashids();
+
 
     @Test
     @DisplayName("Test findById success")
@@ -70,25 +67,13 @@ class SurveyServiceTest {
     }
 
     @Test
-    @Disabled
-    @DisplayName("test insert Survey Response DTO")
-    void testInsertSurveyResponseDto() {
-        var surveyResponseDto = new SurveyResponseDto();
-        List<ElementResponseDto> elementResponseDtos = new ArrayList<>();
-        elementResponseDtos.add(new OpenQuestionResponseDto(9, "This is an answer"));
-        surveyResponseDto.setElementResponseDtos(elementResponseDtos);
-    }
-
-    @Test
     void testGetPublicAndActiveSurveys() {
-        List<Campaign> surveys = List.of(
-                new Campaign().setSurvey(new Survey().setId(1L)),
-                new Campaign().setSurvey(new Survey().setId(2L)));
-        Page<Campaign> surveyPage = new PageImpl<>(surveys);
+        Page<SurveyListElementDto> surveyListElementDtos =
+                new PageImpl<>(List.of(new SurveyListElementDto("aa", "survey1", "desc 1"),
+                        new SurveyListElementDto("bb", "survey2", "desc 2")));
 
-
-        given(campaignRepository.findPublicCampaigns(PageRequest.of(0, 10)))
-                .willReturn(surveyPage);
+        given(campaignService.findPublicCampaigns(PageRequest.of(0, 10)))
+                .willReturn(surveyListElementDtos);
         assertEquals(2, surveyService.getPublicSurveys(PageRequest.of(0, 10)).getTotalElements());
     }
 
